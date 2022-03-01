6bd6ed174f1a5997c1900951916524f42a0b1315
Pasha Khosravi <khosravipasha@users.noreply.github.com>
2022-02-28 22:32:52 -0800

    Update CircuitModelZoo.jl

--- src/CircuitModelZoo.jl
+++ src/CircuitModelZoo.jl
@@ -6,7 +6,7 @@ export zoo_file, zoo_clt_file, zoo_cnf_file, zoo_dnf_file, zoo_jlc_file, zoo_nnf
 
 const ZOO_VERSION = "/Circuit-Model-Zoo-0.1.6"
 
-zoo_file(name, folder) =
+zoo_file(folder, name) =
     artifact"circuit_model_zoo" * ZOO_VERSION * "/$folder/$name"
 
 zoo_clt_file(name) = 
